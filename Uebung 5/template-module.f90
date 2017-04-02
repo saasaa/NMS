1544a8e20613541294728d1c05926b078e8df166
saasaa <saasaa@airmail.cc>
2017-04-02 20:18:12 +0200

    created branch for Uebung 5

---
+++ Uebung 5/template-module.f90
@@ -0,0 +1,21 @@
+module template
+  use iso_fortran_env
+  implicit none
+
+contains
+
+  pure integer (int64) function templ(n)
+
+    integer (int64), intent(in) :: n
+    templ=n*n
+  end function templ
+
+  subroutine test(n)
+    integer (int64), intent(inout) :: n
+    n=n+1
+
+    write(*,*) "hello module"
+
+  end subroutine test
+
+end module template

4511eacff2195fc61f75b69b3cf007967104365f
saasaa <saasaa@airmail.cc>
2017-05-09 12:56:10 +0200

    ue update

--- Uebung 7/functions.f90
+++ Uebung 7/functions.f90
@@ -1,24 +1,50 @@
-module template
-  use iso_fortran_env, only : real32, int64
+module functions
+  use iso_fortran_env, only : real64, int64
   implicit none
 
   private
-  public :: test, templ
+  public :: trapezint
 
 contains
 
-  pure integer (int64) function templ(n)
+  real (real64) function trapezint(array,ndim,startintervall,endintervall,ersterindex,letzterindex) result(res)
+    integer (int64) :: i
+    integer (int64), intent(in) :: ndim
+    integer (int64), optional, intent(in) :: ersterindex,letzterindex
+    real (real64), dimension(ndim), intent(in) :: array
+    real (real64), intent(in) :: startintervall,endintervall
+    integer (int64) :: startwert,endwert
+    real (real64) :: hilf, schrittweite
 
-    integer (int64), intent(in) :: n
-    templ=n*n
-  end function templ
+    if ((present(ersterindex) .AND. present(letzterindex))) then
+       startwert = ersterindex
+       endwert   = letzterindex
+    else if (present(ersterindex)) then
+       startwert = ersterindex
+       endwert = ndim
+    else if (present(letzterindex)) then
+       startwert = 1_int64
+       endwert = letzterindex
+    else
+       startwert = 1_int64
+       endwert   = ndim
+    endif
 
-  subroutine test(n)
-    integer (int64), intent(inout) :: n
-    n=n+1
 
-    write(*,*) "hello module"
+    schrittweite = (endintervall-startintervall)/ndim
 
-  end subroutine test
+    hilf = 0.
 
-end module template
+    do i = startwert, endwert, 1
+       if(i == startwert .OR. i == endwert) then
+          hilf = hilf + array(i)*0.5_real64
+       else
+          hilf = hilf + array(i)
+       end if
+    end do
+
+    res = hilf*schrittweite
+    !
+  end function trapezint
+
+end module functions
--- Uebung 7/ue7bsp1.f90
+++ Uebung 7/ue7bsp1.f90
@@ -1,6 +1,6 @@
 program template
-  use iso_fortran_env, only : real32, int32, int64
-  use functions, only :  !process_file, return_power, write_ft_to_file
+  use iso_fortran_env, only : real32, real64, int64
+  use functions, only :  trapezint !process_file, return_power, write_ft_to_file
 
   implicit none
 
